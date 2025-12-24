8699a34dcdf385ab6e858fe9084e5c9e2f095931
k Bhargava prasad <135947670+bhargav676@users.noreply.github.com>
2025-12-24 17:07:27 +0530

    first

--- app.py
+++ app.py
@@ -1,49 +1,10 @@
-from flask import Flask, request, Response
-import os
-from transformers import pipeline
+from flask import Flask
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-print("Loading fine-tuned model...")
-model_path = "whisper_dysarthria_final"
-
-transcriber = pipeline(
-    "automatic-speech-recognition",
-    model=model_path,
-    device=-1   
-)
-
-print("Model loaded")
-
 @app.route("/")
 def home():
-    return "Flask ASR server running"
+    return "Flask is running"
 
-@app.route("/upload", methods=["POST"])
-def upload_and_transcribe():
-    file_path = None
-    try:
-        if not request.data:
-            return Response("No audio received", status=400)
-
-        file_path = os.path.join(UPLOAD_FOLDER, "audio.wav")
-        with open(file_path, "wb") as f:
-            f.write(request.data)
-
-        result = transcriber(file_path)
-        text = result["text"].strip()
-
-        return Response(text, mimetype="text/plain")
-
-    except Exception as e:
-        return Response(str(e), status=500)
-
-    finally:
-        if file_path and os.path.exists(file_path):
-            os.remove(file_path)
-
-
-
+if __name__ == "__main__":
+    app.run()
